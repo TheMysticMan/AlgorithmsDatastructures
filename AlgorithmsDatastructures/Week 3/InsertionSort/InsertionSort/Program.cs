5615f9b0847186b07a5e727fec3c6beb910b069b
EDO-MSI\Edo <edolimburg@live.nl>
2017-02-28 21:12:49 +0100

    WIP

---
+++ AlgorithmsDatastructures/Week 3/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            arr = Sort(arr);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] Sort(int[] arr)
+        {
+            for (var i = 1; i < arr.Length; i++)
+            {
+                arr = Compare(arr, i, i - 1);
+            }
+            return arr;
+        }
+
+        static int[] Compare(int[] arr, int i, int j)
+        {
+            if (arr[i] < arr[j])
+            {
+                arr = Swap(arr, i, j);
+                if (i > 1)
+                {
+                    arr = Compare(arr, i - 1, j - 1);
+                }
+            }
+            return arr;
+        }
+
+        static int[] Swap(int[] arr, int indexA, int indexB)
+        {
+            var oldA = arr[indexA];
+            arr[indexA] = arr[indexB];
+            arr[indexB] = oldA;
+            return arr;
+        }
+    }
+}
---
+++ AlgorithmsDatastructures/Week 3/Printbackwards/PrintBackwards/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintBackwards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(){0,1,2,3,4,5,6,7,8,9};
+
+            PrintBackwards(list, 3);
+        }
+
+        public static void PrintBackwards(List<int> input, int i)
+        {
+            if (i < input.Count)
+            {
+                PrintBackwards(input, i +1);
+                Console.WriteLine(input[i]);
+                
+            }
+        }
+    }
+}
